e93127de3460f9a6fab8827781618d69cb2c6313
YeceniaQuijada25 <yeceq20@gmail.com>
2025-08-04 07:19:41 -0400

    Creación de la funciónagregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 //Declaración del array amigo
 let amigo=[];
+
+//Función para agregar amigos en el array amigos
+
+function agregarAmigo(){
+
+//Captura el valor de entrada
+
+  let nombre=document.querySelector('#amigo');
+
+
+//Valida la entrada
+  if (nombre.value==""){
+//Envia mensaje de error
+
+    alert("Por favor, inserte un nombre");
+
+  }else{
+//Actualiza el array amigos
+    amigo.push(nombre.value);
+    console.log(amigo);
+    document.querySelector("#amigo").value="";
+    recorrerArray()
+  }
+}
